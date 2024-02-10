2a77456acb73f3c83c122019eb2d3943eb3cb020
Farruh Zoirov <fzoirov29@gmail.com>
2024-02-10 22:13:21 +0500

    i worked with pugs

---
+++ app.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404-error', {prods: adminData.products, pageTitle:'Page not Found'})
+});
+
+app.listen(5000);
---
+++ routes/admin.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+const express = require('express');
+
+
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+const products = [];
+
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+  res.render('adding-product', {pageTitle: 'Add product', path: '/admin/add-product'})
+});
+
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
+});
+
+exports.routes = router;
+exports.products = products;
